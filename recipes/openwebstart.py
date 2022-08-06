d12e029ccee9e0dacfb2a06297e946024245092a
Philipp Wasser <mail@philippwasser.de>
2022-08-06 18:53:28 +0200

    Added openwebstart

---
+++ recipes/openwebstart.py
@@ -0,0 +1,24 @@
+import requests
+
+import urllib.request
+URL = "https://api.github.com/repos/karakun/OpenWebStart/releases/latest"
+
+
+
+def check_version(current_version):
+    resp=requests.get(URL)
+    data=resp.json()
+    new_version=data['tag_name'][1:]
+    print("Current version: " + current_version)
+    print("Latest version: " + new_version)
+    if new_version == current_version:
+        return None
+    else:
+        for asset in data['assets']:
+            if asset['name'] == 'OpenWebStart_linux_{version}.deb'.format(version=new_version.replace('.','_')):
+                return (new_version, asset['browser_download_url'],"openwebstart_{version}_amd64.deb".format(version=new_version))
+        return None
+
+
+def build_package(version,location):
+    urllib.request.urlretrieve(version[1], location+"/"+version[2])
