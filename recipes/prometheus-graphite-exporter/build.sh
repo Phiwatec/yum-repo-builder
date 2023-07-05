73dc5a78ebbedeb3036aa935bc3840217d775723
user <user@pihole.home.pwserv.net>
2023-07-05 21:25:43 +0200

    Current version

---
+++ recipes/prometheus-graphite-exporter/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Getting File"
+wget $1 -q -O  /tmp/working.deb > /dev/null
+echo "Making temp dir"
+mkdir /tmp/debbuild
+echo "unpacking deb"
+touch /tmp/PRE
+dpkg-deb -R /tmp/working.deb /tmp/debbuild
+touch /tmp/AFTER
+echo "Making changes"
+echo "Maintainer: Phiwatec" > /tmp/debbuild/DEBIAN/control
+echo "Package: prometheus-ping-exporter" >> /tmp/debbuild/DEBIAN/control
+echo "Version: ${2}" >> /tmp/debbuild/DEBIAN/control
+echo "Architecture: ${4}" >> /tmp/debbuild/DEBIAN/control
+echo "Description: Prometheus Ping Exporter" >> /tmp/debbuild/DEBIAN/control 
+echo "Packing deb"
+dpkg-deb -b /tmp/debbuild /tmp/working.deb
+echo "Copying.."
+mv /tmp/working.deb $3
+rm -r /tmp/debbuild
+echo "Done :)"
