8692a95ad79d6c443cff4338b7a3004c8f1b7058
Philipp Wasser <52720981+Phiwatec@users.noreply.github.com>
2023-07-06 12:01:34 +0200

    Add arm64

--- recipes/step-cli.py
+++ recipes/step-cli.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import requests
 import urllib.request
-
+ARCH=['amd64','arm64']
 URL = "https://api.github.com/repos/smallstep/cli/releases/latest"
 
 load_dotenv()
@@ -11,21 +11,33 @@ username = os.environ.get('GH_USER')
 token = os.environ.get('GH_TOKEN')
 auth = (username, token)
 
-
 def check_version(current_version):
     resp = requests.get(URL, auth=auth)
     data = resp.json()
     new_version = data['tag_name'][1:]
     print("Current version: " + current_version)
     print("Latest version: " + new_version)
+    returndata = []
     if new_version == current_version:
         return (None, None)
     else:
-        for asset in data['assets']:
-            if asset['name'] == f'step-cli_{new_version}_amd64.deb':
-                return (new_version, (asset['browser_download_url'], asset['name']))
-        return (None, None)
+        for arch in ARCH:
+            for asset in data['assets']:
+                if asset['name'] == f'step-cli_{new_version}_{arch}.deb':
+
+                    returndata.append(
+                        (asset['browser_download_url'], asset['name'], arch))
+
+        if len(returndata) == 0:
+            print("Error! New version but no matching assets found")
+            return (None, None)
+
+        else:
+            return (new_version, returndata)
+
+
 
 
 def build_package(version, location, data):
-    urllib.request.urlretrieve(data[0], location+"/"+data[1])
+    for arch in data:
+        urllib.request.urlretrieve(arch[0], location+"/"+arch[1])
